5c2f0714f26bd99b505b3113091781cf29a6130e
Andreas Donig <git@innwiese.de>
2024-05-15 09:55:55 +0200

    Add the backtracking engine implementation

--- lib/backfish.ex
+++ lib/backfish.ex
@@ -1,18 +1,42 @@
 defmodule Backfish do
-  @moduledoc """
-  Documentation for `Backfish`.
+  @doc """
+  Finds all solutions to the given problem.
   """
+  def find_all_solutions(problem_module) do
+    initial_state = problem_module.initial_state()
+    solve_all(problem_module, initial_state, [])
+  end
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Backfish.hello()
-      :world
-
+  Finds the first valid solution to the given problem.
   """
-  def hello do
-    :world
+  def find_first_solution(problem_module) do
+    initial_state = problem_module.initial_state()
+    solve_first(problem_module, initial_state, [])
+  end
+
+  defp solve_all(problem_module, state, path) do
+    if problem_module.is_goal?(state, path) do
+      [Enum.reverse(path)]
+    else
+      problem_module.next_steps(state, path)
+      |> Enum.flat_map(fn next_state ->
+        solve_all(problem_module, next_state, [next_state | path])
+      end)
+    end
+  end
+
+  defp solve_first(problem_module, state, path) do
+    if problem_module.is_goal?(state, path) do
+      {:ok, Enum.reverse(path)}
+    else
+      problem_module.next_steps(state, path)
+      |> Enum.find_value(fn next_state ->
+        case solve_first(problem_module, next_state, [next_state | path]) do
+          {:ok, solution} -> {:ok, solution}
+          :error -> nil
+        end
+      end) || :error
+    end
   end
 end
