fa2a8da61325ae00bc186b5571f65bcf9ff8c04a
Andreas Donig <git@innwiese.de>
2024-05-15 09:56:12 +0200

    Test the backtracking engine implementation

--- test/backfish_test.exs
+++ test/backfish_test.exs
@@ -1,8 +1,21 @@
 defmodule BackfishTest do
-  use ExUnit.Case
-  doctest Backfish
+  use ExUnit.Case, async: true
 
-  test "greets the world" do
-    assert Backfish.hello() == :world
+  defmodule TestProblem do
+    use Backfish.ProblemDefinition
+
+    def initial_state, do: 0
+    def is_goal?(state, _path), do: state == 3
+    def next_steps(state, _path), do: [state + 1]
+  end
+
+  test "find_all_solutions finds all solutions" do
+    solutions = Backfish.find_all_solutions(TestProblem)
+    assert solutions == [[1, 2, 3]]
+  end
+
+  test "find_first_solution finds the first solution" do
+    {:ok, solution} = Backfish.find_first_solution(TestProblem)
+    assert solution == [1, 2, 3]
   end
 end
