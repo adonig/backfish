0b58c0b7c4afc210792d2bfc338cb4a52c4a0a7b
Andreas Donig <git@innwiese.de>
2024-05-18 12:07:37 +0200

    Add ExDoc to generate documentation

--- mix.exs
+++ mix.exs
@@ -7,7 +7,16 @@ defmodule Backfish.MixProject do
       version: "0.1.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      # ExDoc
+      name: "Backfish",
+      source_url: "https://github.com/adonig/backfish",
+      homepage_url: "https://github.com/adonig/backfish",
+      docs: [
+        main: "Backfish",
+        logo: "logo.png",
+        extras: ["LICENSE", "README.md"]
+      ]
     ]
   end
 
@@ -21,8 +30,7 @@ defmodule Backfish.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.31", only: :dev, runtime: false}
     ]
   end
 end
